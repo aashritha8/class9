cc04023456dc865dfcfe9dd22c38bf0c3c4ffe7f
aashritha8 <124457104+aashritha8@users.noreply.github.com>
2023-02-04 13:01:22 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+
+function setup() {
+  createCanvas(400,400);
+  box=createSprite(300,200)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites()
+if (keyDown("right")){
+box.shapeColor="red"
+}
+}
+
+
+
+
